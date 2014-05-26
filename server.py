bf490814898d227154c2c53d4bc9c04e4221c381
Luca Bacchi <luca@ovus.it>
2014-05-26 17:55:43 +0200

    multiprocessing version
    	modified:   server.py

--- server.py
+++ server.py
@@ -11,10 +11,10 @@ Luca Bacchi <bacchilu@gmail.com> - http://www.lucabacchi.it
 
 import BaseHTTPServer
 import socket
-import threading
+import multiprocessing
 
 
-class MyHandler(BaseHTTPServer.BaseHTTPRequestHandler):
+class GetHandler(BaseHTTPServer.BaseHTTPRequestHandler):
 
     def do_GET(self):
         self.send_response(200)
@@ -23,16 +23,21 @@ class MyHandler(BaseHTTPServer.BaseHTTPRequestHandler):
         self.wfile.write('OK')
         self.wfile.close()
 
+    def log_message(self, format, *args):
+        import sys
+        sys.stderr.write('%s - - [%s] %s\n' % (self.client_address[0],
+                         self.log_date_time_string(), format % args))
+
 
 class StoppableHTTPServer(BaseHTTPServer.HTTPServer):
 
     def server_bind(self):
         BaseHTTPServer.HTTPServer.server_bind(self)
         self.socket.settimeout(1)
-        self.run = True
+        self.exit = multiprocessing.Event()
 
     def get_request(self):
-        while self.run:
+        while not self.exit.is_set():
             try:
                 (sock, addr) = self.socket.accept()
                 sock.settimeout(None)
@@ -41,15 +46,17 @@ class StoppableHTTPServer(BaseHTTPServer.HTTPServer):
                 pass
 
     def stop(self):
-        self.run = False
+        self.exit.set()
 
     def serve(self):
-        while self.run:
+        while not self.exit.is_set():
             self.handle_request()
 
 
 if __name__ == '__main__':
-    httpd = StoppableHTTPServer(('127.0.0.1', 8081), MyHandler)
-    threading.Thread(target=httpd.serve).start()
+    httpd = StoppableHTTPServer(('127.0.0.1', 8080), GetHandler)
+    p = multiprocessing.Process(target=httpd.serve)
+    p.start()
     raw_input('Press <RETURN> to stop server\n')
     httpd.stop()
+    p.join()
