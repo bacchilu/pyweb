e28c4acb8f1c86b3cacecad724073ae62933b5a0
Luca Bacchi <luca@ovus.it>
2014-05-30 18:19:53 +0200

    Refactoring
    	modified:   main.py

--- main.py
+++ main.py
@@ -10,20 +10,49 @@ Luca Bacchi <bacchilu@gmail.com> - http://www.lucabacchi.it
 """
 
 import multiprocessing
+import cmd
 
 import web
 import controller
 
+
+class ServiceManager(object):
+
+    @classmethod
+    def startAll(cls):
+        cls.q = multiprocessing.Queue()
+        web.WebServer.start(cls.q)
+        controller.Consumer.start(cls.q)
+
+    @classmethod
+    def stopAll(cls):
+        web.WebServer.stop()
+        controller.Consumer.stop()
+        cls.q.close()
+        cls.q.join_thread()
+
+
+class Commander(cmd.Cmd):
+
+    def do_start(self, service):
+        """start [service]
+        Avvia tutti i servizi o un servizio
+        service={web|controller}"""
+
+        ServiceManager.startAll()
+
+    def do_stop(self, line):
+        """stop [service]
+        Arresta tutti i servizi o un servizio"""
+
+        ServiceManager.stopAll()
+
+    def do_exit(self, line):
+        """Esce"""
+
+        return True
+
+
 if __name__ == '__main__':
-    q = multiprocessing.Queue()
-
-    web.WebServer.start(q)
-    controller.Consumer.start(q)
-
-    raw_input('Press <RETURN> to stop server\n')
-
-    web.WebServer.stop()
-    controller.Consumer.stop()
-
-    q.close()
-    q.join_thread()
+    print 'help per una lista dei comandi'
+    Commander().cmdloop()
