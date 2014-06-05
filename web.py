8ee824a7d092dd019aefea929e348ac7a1266091
Luca Bacchi <luca@ovus.it>
2014-06-05 12:36:33 +0200

    Sistemati problemi sul web server
    	modified:   web.py

--- web.py
+++ web.py
@@ -36,8 +36,6 @@ class StoppableHTTPServer(BaseHTTPServer.HTTPServer):
     def server_bind(self):
         BaseHTTPServer.HTTPServer.server_bind(self)
         self.socket.settimeout(1)
-        self.exit = multiprocessing.Event()
-        self.close = multiprocessing.Event()
 
     def get_request(self):
         while not self.exit.is_set():
@@ -48,33 +46,12 @@ class StoppableHTTPServer(BaseHTTPServer.HTTPServer):
             except socket.timeout:
                 pass
 
-    def stop(self):
-        self.exit.set()
-        self.close.wait()
-        self.server_close()
-
     def serve(self, q):
         self.cmdQueue = q
         while not self.exit.is_set():
             self.handle_request()
         self.cmdQueue.close()
         self.cmdQueue.join_thread()
-        self.close.set()
-
-
-class WebServerProcess(multiprocessing.Process):
-
-    def __init__(self, q):
-        multiprocessing.Process.__init__(self)
-        self.httpd = StoppableHTTPServer(('127.0.0.1', 8080),
-                GetHandler)
-        self.q = q
-
-    def run(self):
-        self.httpd.serve(self.q)
-
-    def stop(self):
-        self.httpd.stop()
 
 
 class WebServer(object):
@@ -82,11 +59,19 @@ class WebServer(object):
     p = None
 
     @classmethod
+    def worker(cls, exit, q):
+        httpd = StoppableHTTPServer(('127.0.0.1', 8080), GetHandler)
+        httpd.exit = exit
+        httpd.serve(q)
+
+    @classmethod
     def start(cls, q):
         if cls.p is not None and cls.p.is_alive():
             return
 
-        cls.p = WebServerProcess(q)
+        cls.exit = multiprocessing.Event()
+        cls.p = multiprocessing.Process(target=cls.worker,
+                args=(cls.exit, q))
         cls.p.start()
 
     @classmethod
@@ -94,7 +79,7 @@ class WebServer(object):
         if cls.p is None or not cls.p.is_alive():
             return
 
-        cls.p.stop()
+        cls.exit.set()
         cls.p.join()
 
     @classmethod
