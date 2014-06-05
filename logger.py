04d065971015ab675481da792e3c8f67d48aab4f
Luca Bacchi <luca@ovus.it>
2014-06-05 18:54:32 +0200

    Logger
    	modified:   logger.py

--- logger.py
+++ logger.py
@@ -4,10 +4,15 @@
 """
 logger.py - https://github.com/bacchilu/pyweb
 
+Il logger è in pratica un thread che attende i messaggi da loggare da una coda.
+Non implemento nessun meccanismo particolarmente sofisticato per la chiusura del
+thread: daemon=True.
+
 Luca Bacchi <bacchilu@gmail.com> - http://www.lucabacchi.it
 """
 
 import multiprocessing
+import threading
 import logging
 
 FORMAT = \
@@ -16,7 +21,20 @@ logging.basicConfig(filename='logger.log', level=logging.DEBUG,
                     format=FORMAT)
 
 
+def loggerWorker(q):
+    while True:
+        (level, msg) = q.get()
+        if level == 'debug':
+            logging.debug(msg)
+
+
+q = multiprocessing.Queue()
+t = threading.Thread(target=loggerWorker, args=(q, ))
+t.daemon = True
+t.start()
+
+
 def debug(msg):
-    logging.debug(msg)
+    q.put(('debug', msg))
 
 
